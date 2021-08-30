fd0169ec9ef3d31ee0bfb721b1930f0e716cb063
acech <wahcen@163.com>
2021-08-30 23:48:39 +0800

    更新springboot版本到2.4.10, springcloud版本到2020.0.3
    springboot-parent版本低于2.2.0时, actuator与openfeign不兼容
    具体体现在ClassNotFoundException:CompositeHealthContributor
    低版本actuator没有该类, feign的DiscoveryCompositeHealthContributor实现了该接口
    
    Signed-off-by: acech <wahcen@163.com>

--- src/test/java/com/acech/demo/ApplicationTest.java
+++ src/test/java/com/acech/demo/ApplicationTest.java
@@ -1,14 +1,15 @@
 package com.acech.demo;
 
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @author wahcen@163.com
  * @date 2021/8/29 2:39
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 public class ApplicationTest {
 }
